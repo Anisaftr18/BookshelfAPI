7b03972640a2f739af4470a36609b913ecaacd35
Anisaftr18 <anisafitrianida75@gmail.com>
2022-11-17 19:53:24 +0700

    first

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+const {
+  postBookHandler,
+  getAllBooksHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: postBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId?}',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
